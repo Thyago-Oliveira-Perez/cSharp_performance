db40dfa54c2556ac7638a266e013bd62b219d144
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-04-21 13:17:37 -0300

    feat: init frontend

---
+++ webApp/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import RouterConfig from "./routes/router-config";
+function App() {
+  return (
+    <BrowserRouter>
+      <RouterConfig />
+    </BrowserRouter>
+  );
+}
+
+export default App;
