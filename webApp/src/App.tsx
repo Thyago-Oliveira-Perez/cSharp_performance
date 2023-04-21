85aec2213625c51ca08e4e2bcd97d8478503c8c3
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-04-21 14:49:49 -0300

    feat: add position to the header and footer

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import RouterConfig from "./routes/router-config";
-import { Header } from "./components/header";
-import { Footer } from "./components/footer";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <RouterConfig />
-      <Footer />
     </BrowserRouter>
   );
 }
--- webApp/src/routes/router-config.tsx
+++ webApp/src/routes/router-config.tsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/home";
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
+import styles from "./styles.module.css";
 
 export default function RouterConfig() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <div className={styles.routes}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
