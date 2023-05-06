faeb11dbe0710adbba21ea8ed0af5c66ef6d01db
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-05-06 09:34:28 -0300

    fix: dockerfile

--- api/api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ api/api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6d978999-4354-46c8-8e6f-8b38cf52a2e4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
